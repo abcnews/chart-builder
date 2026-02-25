263fb0e380b23f48ddb119a991662ca9a8f0d8cb
Simon Elvery <simon@elvery.net>
2026-02-26 09:27:17 +1000

    Experiment with row parser types

--- src/lib/data-helpers.ts
+++ src/lib/data-helpers.ts
@@ -8,43 +8,43 @@ import type { ColumnDefinitionType } from './types';
  * It will return an infered type or, if the passed column definitions specifies a specific type it will attempt to
  * parse that type for the given column and return `null` if not possible.
  */
-export const rowParser = (columnDefinitions: ColumnDefinitionType) => (row: DSVRowString<string>) => {
-  const result: Record<string, string | boolean | Date | number | null> = {};
-  for (let columnName in row) {
-    const specified = columnDefinitions[columnName];
-    let value: string | boolean | Date | number | null = row[columnName].trim();
-    let number: number;
-    let m: RegExpMatchArray | null;
+export const rowParser =
+  <T extends keyof ColumnDataTypeMap1>(columnDefinitions: Record<string, T>) =>
+  (row: DSVRowString<string>) => {
+    const result: Record<string, ColumnDataTypeMap1[T] | null> = {};
+    for (let columnName in row) {
+      const specified = columnDefinitions[columnName];
+      let input: string | boolean | Date | number | null = row[columnName] ? row[columnName].trim() : '';
+      let value: ColumnDataTypeMap1[T] | null = null;
+      let number: number;
+      let m: RegExpMatchArray | null;
 
-    const dateRegex = new RegExp(
-      /^([-+]\d{2})?\d{4}(-\d{2}(-\d{2})?)?(T\d{2}:\d{2}(:\d{2}(\.\d{3})?)?(Z|[-+]\d{2}:\d{2})?)?$/
-    );
+      const dateRegex = new RegExp(
+        /^([-+]\d{2})?\d{4}(-\d{2}(-\d{2})?)?(T\d{2}:\d{2}(:\d{2}(\.\d{3})?)?(Z|[-+]\d{2}:\d{2})?)?$/
+      );
 
-    if (specified) {
-      if (specified === 'string') value = value;
-      else if (specified === 'boolean') value = value === 'true' ? true : value === 'false' ? (value = false) : null;
-      else if (specified === 'date') {
-        if ((m = value.match(dateRegex))) {
-          if (fixtz && !!m[4] && !m[7]) value = value.replace(/-/g, '/').replace(/T/, ' ');
-          value = new Date(value);
-        } else return null;
-      } else if (specified === 'number') value = !isNaN((number = +value)) ? number : null;
-    } else {
-      if (!value) value = null;
-      else if (value === 'true') value = true;
-      else if (value === 'false') value = false;
-      else if (value === 'NaN') value = NaN;
-      else if (!isNaN((number = +value))) value = number;
-      else if ((m = value.match(dateRegex))) {
-        if (fixtz && !!m[4] && !m[7]) value = value.replace(/-/g, '/').replace(/T/, ' ');
-        value = new Date(value);
-      } else continue;
+      if (specified) {
+        if (specified === 'string') value = input as ColumnDataTypeMap1[T];
+        else if (specified === 'boolean') {
+          if (input === 'true') value = true as ColumnDataTypeMap1[T];
+          else if (input === 'false') value = false as ColumnDataTypeMap1[T];
+          else value = null;
+        } else if (specified === 'date') {
+          if ((m = input.match(dateRegex))) {
+            if (fixtz && !!m[4] && !m[7]) input = input.replace(/-/g, '/').replace(/T/, ' ');
+            value = new Date(input) as ColumnDataTypeMap1[T];
+          } else return null;
+        } else if (specified === 'number') {
+          if (!isNaN((number = +input))) {
+            value = number as ColumnDataTypeMap1[T];
+          } else value = null;
+        }
+      }
+
+      result[columnName] = value;
     }
-
-    result[columnName] = value;
-  }
-  return result;
-};
+    return result;
+  };
 
 // https://github.com/d3/d3-dsv/issues/45
 const fixtz = new Date('2019-01-01T00:00').getHours() || new Date('2019-07-01T00:00').getHours();
